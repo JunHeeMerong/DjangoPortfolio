93291ea2d683c777e291e7b8dd2657eca22cfd71
이준희 <reborn37@naver.com>
2023-04-17 23:00:31 +0900

    서버연결시도

--- config/settings.py
+++ config/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'reborn37',
+        'PASSWORD':'anjgkwl12.',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
