87da7613a053cb304dcd6f62e35b7e8be9682b5e
이준희 <reborn37@naver.com>
2023-04-17 23:04:35 +0900

    데이터베이스 서버실행내용 변경

--- config/settings.py
+++ config/settings.py
@@ -79,10 +79,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'reborn37',
-        'PASSWORD':'anjgkwl12.',
-        'HOST': 'localhost',
-        'PORT': '',
     }
 }
 
